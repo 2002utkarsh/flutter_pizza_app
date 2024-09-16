0391e6dcb903510aa888526489d631ddf4fd1be8
INFECTRON <utkarsh.gupta@ucalgary.ca>
2024-09-15 21:34:39 -0600

    init, for the pizza lrn app. Creates user class.

---
+++ packages/user_repo/lib/src/firebase_user_repo.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:user_repo/src/user_repo.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+
+class FirebaseUserRepo implements UserRepo{
+  final FirebaseAuth _firebaseAuth;
+  final usersCollection = FirebaseFirestore.instance.collection('users');
+
+  FirebaseUserRepo({
+    FirebaseAuth? firebaseAuth,
+  }) : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
+  
+  @override
+  Future<void> logOut() {
+    // TODO: implement logOut
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<void> setUserData(user) {
+    // TODO: implement setUserData
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<void> signIn(String email, String pass) {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+  
+  @override
+  Future<dynamic> signUp(myUser, String pass) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+  
+  @override
+  // TODO: implement user
+  Stream<dynamic> get user => throw UnimplementedError();
+}
+
---
+++ packages/user_repo/lib/src/user_repo.dart
@@ -0,0 +1,11 @@
+abstract class UserRepo {
+  Stream<MyUser> get user;
+
+  Future<MyUser> signUp(MyUser myUser, String pass);
+
+  Future<void> setUserData (MyUser user);
+
+  Future<void> signIn(String email, String pass);
+
+  Future<void> logOut();
+}
